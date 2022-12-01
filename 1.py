feb11a3c8d54392ce58f317245a8d3f3c61d1de0
Markus Günther <markus.m.guenther-extern@deutschebahn.com>
2022-12-01 10:00:21 +0100

    Refines solution for day 1, adds README

--- 1.py
+++ 1.py
@@ -2,21 +2,17 @@ lines = [line.strip() for line in open('1.in', 'r').readlines()] + ['']
 
 elf = 1
 calories_acc = 0
-calories_per_elf = {}
+calories_by_elf = {}
 
-for l in lines:
-    if l == '':
-        calories_per_elf[elf] = calories_acc
+for line in lines:
+    if line == '':
+        calories_by_elf[elf] = calories_acc
         elf += 1
         calories_acc = 0
         continue
-    calories_acc += int(l)
+    calories_acc += int(line)
 
-print(calories_per_elf)
+highest_calories = sorted(calories_by_elf.items(), key=lambda x: x[1], reverse=True)[:3]
 
-elf_with_most_calories = max(calories_per_elf, key=calories_per_elf.get)
-
-print(elf_with_most_calories, calories_per_elf[elf_with_most_calories])
-
-highest_calories = sorted(calories_per_elf.items(), key=lambda x: x[1], reverse=True)[:3]
+print(calories_by_elf[max(calories_by_elf, key=calories_by_elf.get)])
 print(sum([calories[1] for calories in highest_calories]))
---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2022
+
+This repository contains my solutions for the Advent of Code season 2022. My programming language of choice was again Python this year. I didn't bother using PyPy, but went with vanilla CPython. All solutions compute in a reasonable amount of time.
+
+## License
+
+This work is released under the terms of the Apache 2.0 license.
