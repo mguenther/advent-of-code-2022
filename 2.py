bc533f25ac8581fcb6a46c12aaa9a3dc601b1af5
Markus Günther <markus.m.guenther-extern@deutschebahn.com>
2022-12-02 09:26:40 +0100

    Refines solution for day 2

--- 2.py
+++ 2.py
@@ -2,43 +2,18 @@ from itertools import permutations
 
 
 FILENAME = '2.in'
-
-MAPPING = {
-    'A': 'Rock',
-    'B': 'Paper',
-    'C': 'Scissors'
-}
-
-TURN_SCORING = {
-            ('A', 'A'): 1 + 3,
-            ('A', 'B'): 1 + 0,
-            ('A', 'C'): 1 + 6,
-            ('B', 'A'): 2 + 6,
-            ('B', 'B'): 2 + 3,
-            ('B', 'C'): 2 + 0,
-            ('C', 'A'): 3 + 0,
-            ('C', 'B'): 3 + 6,
-            ('C', 'C'): 3 + 3
-}
-
-moves = [move.strip().split(' ') for move in open(FILENAME, 'r').readlines()]
-
-MAP = {
+# A maps to Rock
+# B maps to Paper
+# C maps to Scissors
+STRATEGY = {
     'Y': 'B',
     'X': 'A',
     'Z': 'C'
 }
-
-score = 0
-
-for move in moves:
-    opponent_move = move[0]
-    our_move = MAP[move[1]]
-    score += TURN_SCORING[(our_move, opponent_move)]
-
-print(MAP, score)
-
-MAP_2 = {
+# X means we are expected to lose
+# Y means we are expected to end in a draw
+# Z means we are expected to win
+STRATEGY_REFINED = {
     ('A', 'X'): 'C',
     ('B', 'X'): 'A',
     ('C', 'X'): 'B',
@@ -49,27 +24,45 @@ MAP_2 = {
     ('B', 'Z'): 'C',
     ('C', 'Z'): 'A'
 }
+SCORING = {
+    ('A', 'A'): 1 + 3,
+    ('A', 'B'): 1 + 0,
+    ('A', 'C'): 1 + 6,
+    ('B', 'A'): 2 + 6,
+    ('B', 'B'): 2 + 3,
+    ('B', 'C'): 2 + 0,
+    ('C', 'A'): 3 + 0,
+    ('C', 'B'): 3 + 6,
+    ('C', 'C'): 3 + 3
+}
 
-score_2 = 0
 
-for move in moves:
-    opponent_move = move[0]
-    our_strategy = move[1]
-    our_move = MAP_2[(opponent_move, our_strategy)]
-    score_2 += TURN_SCORING[(our_move, opponent_move)]
+def solve_first_part(moves):
 
-print(score_2)
+    score = 0
 
-# (A,X) -> C
-# (B,X) -> A
-# (C,X) -> B
-# (A,Y) -> A
-# (B,Y) -> B
-# (C,Y) -> C
-# (A,Z) -> B
-# (B,Z) -> C
-# (C,Z) -> A
+    for move in moves:
+        opponent_move = move[0]
+        our_move = STRATEGY[move[1]]
+        score += SCORING[(our_move, opponent_move)]
 
-# X -> lose
-# Y -> draw
-# Z -> win
+    print(score)
+
+
+def solve_second_part(moves):
+
+    score = 0
+
+    for move in moves:
+        opponent_move = move[0]
+        our_strategy = move[1]
+        our_move = STRATEGY_REFINED[(opponent_move, our_strategy)]
+        score += SCORING[(our_move, opponent_move)]
+
+    print(score)
+
+
+moves = [move.strip().split(' ') for move in open(FILENAME, 'r').readlines()]
+
+solve_first_part(moves)
+solve_second_part(moves)
