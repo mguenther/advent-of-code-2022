781795884fb1ed291e58b8f1b5ff389c0597bfc1
Markus Günther <markus.m.guenther-extern@deutschebahn.com>
2022-12-06 11:44:15 +0100

    Refines solution for day 6

--- 6.py
+++ 6.py
@@ -2,11 +2,13 @@ FILENAME = '6.in'
 
 
 def find_marker(sequence, window_size):
+    marker = (None, None)
     for i in range(0, len(sequence) - window_size + 1):
         window = sequence[i:i+window_size]
         if all(window.count(c) == 1 for c in window):
-            return (i + window_size, sequence[i:i+window_size])
-    return (None, None)
+            marker = (i + window_size, sequence[i:i+window_size])
+            break
+    return marker
 
 
 sequence = [l.strip() for l in open(FILENAME, 'r').readlines()][0]
