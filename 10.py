c5619bf839a458f63a7b1762a47ba4852115d504
Markus Günther <markus.guenther@gmail.com>
2022-12-10 15:28:28 +0100

    Cleans up solution for day 10 a bit. There is still a one-off problem in the code but I've lost interest in fixing it.

--- 10.py
+++ 10.py
@@ -1,51 +1,46 @@
 FILENAME = '10.in'
+PROBE_AT_CYCLE = [20, 60, 100, 140, 180, 220]
+
+
+def row(cycle):
+    return cycle // 40
+
+
+def column(cycle):
+    return cycle % 40
+
+
+def pixel(cycle, sprite_pos):
+    return '#' if (cycle % 40) in sprite_pos else ' '
 
 
 program = [l.strip() for l in open(FILENAME, 'r').readlines()]
 signal_strengths = []
-pixels = []
+pixels = [['X' for _ in range(40)] for _ in range(7)]
 sprite_pos = [1, 2, 3]
 x = 1
 cycle = 0
-wait = 0
-wait_buffer = 0
 for statement in program:
     already = False
     if statement.startswith('noop'):
         cycle += 1
-        if (cycle % 40) in sprite_pos:
-            pixels.append('#')
-        else:
-            pixels.append('.')
+        pixels[row(cycle)][column(cycle)] = pixel(cycle, sprite_pos)
     elif statement.startswith('addx'):
         command, argument = statement.split()
         argument = int(argument)
-        wait = 2
-        wait_buffer = argument
-        while wait > 0:
+        for _ in range(2):
             cycle += 1
-            wait -= 1
-            if (cycle % 40) in sprite_pos:
-                pixels.append('#')
-            else:
-                pixels.append('.')
-            if cycle > 0 and ((cycle - 20) % 40 == 0 or cycle == 20):
+            pixels[row(cycle)][column(cycle)] = pixel(cycle, sprite_pos)
+            if cycle in PROBE_AT_CYCLE:
                 signal_strengths.append(cycle * x)
                 already = True
-        x += wait_buffer
-        wait_buffer = 0
+        x += argument
         sprite_pos = [x, x + 1, x + 2]
-    if not(already) and (cycle > 0 and ((cycle - 20) % 40 == 0 or cycle == 20)):
+    if not already and cycle in PROBE_AT_CYCLE:
         signal_strengths.append(cycle * x)
-        if (cycle % 40) in sprite_pos:
-            pixels.append('#')
-        else:
-            pixels.append('.')
+        pixels[row(cycle)][column(cycle)] = pixel(cycle, sprite_pos)
 
 print(sum(signal_strengths))
 
-for i, c in enumerate(pixels):
-    if i > 0 and (i % 40) == 0:
-        print()
-    print(c, end='')
-# prints PZGPKPEB
+for row in pixels:
+    print(''.join(row))
