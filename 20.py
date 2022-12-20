3fc20d39aeb2e8e9f90c1dfdc78162bee526ac75
= <markus.guenther@gmail.com>
2022-12-20 13:29:51 +0100

    Refines solution for day 20

--- 20.py
+++ 20.py
@@ -13,30 +13,25 @@ def get(i, n):
 def solve(rounds = 1, multiplier = 1):
     numbers = [(i, int(l.strip()) * multiplier) for i, l in enumerate(open(FILENAME, 'r').readlines())]
     length = len(numbers)
-
     for _ in range(rounds):
         for i in range(length):
-            t = None
-            ii = None
+            indexed_element, original_index = None, None
             for k in range(length):
                 if numbers[k][0] == i:
-                    t = numbers[k]
-                    ii = k
+                    indexed_element = numbers[k]
+                    original_index = k
                     break
-
             # if the value is 0, we're doing nothing
-            if t[1] == 0:
+            # (this is just for the sake cleanliness, the algorithm
+            # works correctly without that step)
+            if indexed_element[1] == 0:
                 continue
-
-            # otherwise, we'll determine the new index position
-            j = (k + t[1]) % (length - 1)
-            del numbers[ii]
-            numbers.insert(j, t)
-
-    a = get(1000, numbers)
-    b = get(2000, numbers)
-    c = get(3000, numbers)
-
+            # otherwise, we'll determine the new index position,
+            # remove the element from the old position and insert
+            # it into the list at the new position
+            j = (k + indexed_element[1]) % (length - 1)
+            del numbers[original_index]
+            numbers.insert(j, indexed_element)
     return get(1000, numbers) + get(2000, numbers) + get(3000, numbers)
 
 
