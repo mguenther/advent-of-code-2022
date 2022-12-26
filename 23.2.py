52a03348e722995e10329e3fbf2fac53e26e8ebf
Markus Günther <markus.guenther@gmail.com>
2022-12-26 20:46:58 +0100

    Adds solution to day 23

---
+++ 23.2.py
@@ -0,0 +1,72 @@
+FILENAME = '23.in'
+POSSIBLE_MOVES = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+NEIGHBOURS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+CLEARANCE_BY_MOVE = {
+    (0, -1): [(-1, -1), (0, -1), (1, -1)], # check NW, N, NE in order to propose a move to N
+    (0, 1) : [(-1, 1), (0, 1), (1, 1)],    # check SW, S, SE in order to propose a move to S
+    (-1, 0): [(-1, 0), (-1, -1), (-1, 1)], # check W, NW, SW in order to propose a move to W
+    (1, 0) : [(1, 0), (1, -1), (1, 1)],    # check E, NE, SE in order to propose a move to E
+}
+
+
+def parse(filename):
+    lines = [l.strip() for l in open(filename, 'r').readlines()]
+    elves = set()
+    for row, line in enumerate(lines):
+        for col, field in enumerate(line):
+            if field == '#':
+                elves.add((col, row))
+    return elves
+
+
+elves = parse(FILENAME)
+last_state = set(elves)
+i = 1
+
+while True:
+    once = set()
+    twice = set()
+    for elf in elves:
+        abs_neighbours = [(elf[0] + dx, elf[1] + dy) for (dx, dy) in NEIGHBOURS]
+        is_isolated = all(neighbour not in elves for neighbour in abs_neighbours)
+        if is_isolated:
+            continue
+        for move in POSSIBLE_MOVES:
+            probe_moves = [(elf[0] + dx, elf[1] + dy) for (dx, dy) in CLEARANCE_BY_MOVE[move]]
+            is_clear = all(np not in elves for np in probe_moves)
+            if is_clear:
+                prop = (elf[0] + move[0], elf[1] + move[1])
+                if prop in twice:
+                    pass
+                elif prop in once:
+                    twice.add(prop)
+                else:
+                    once.add(prop)
+                break
+
+    elves_clone = set(elves)
+
+    for elf in elves_clone:
+        abs_neighbours = [(elf[0] + dx, elf[1] + dy) for (dx, dy) in NEIGHBOURS]
+        is_isolated = all(neighbour not in elves_clone for neighbour in abs_neighbours)
+        if is_isolated:
+            continue
+        for move in POSSIBLE_MOVES:
+            probe_moves = [(elf[0] + dx, elf[1] + dy) for (dx, dy) in CLEARANCE_BY_MOVE[move]]
+            is_clear = all(np not in elves_clone for np in probe_moves)
+            if is_clear:
+                prop = (elf[0] + move[0], elf[1] + move[1])
+                if prop not in twice:
+                    elves.remove(elf)
+                    elves.add(prop)
+                break
+    
+    POSSIBLE_MOVES.append(POSSIBLE_MOVES.pop(0))
+
+    if last_state == elves:
+        break
+
+    last_state = set(elves)
+    i += 1
+
+print(i)
